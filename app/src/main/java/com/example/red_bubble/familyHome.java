a2189bc2e5af0ebffc77e3c867f16c20901d92c1
samba2018 <samudithahasantha12@gmail.com>
2021-08-14 18:51:10 +0530

    new button add

---
+++ app/src/main/java/com/example/red_bubble/familyHome.java
@@ -0,0 +1,14 @@
+package com.example.red_bubble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class familyHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family_home);
+    }
+}
